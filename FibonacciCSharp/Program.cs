a0aa6458082b92374686ee3cf301fcf04f13e032
Manuel J. González Rodríguez <majegonro@gmail.com>
2015-07-19 16:55:14 +0200

    Commit inicial: proyecto c# en consola. funciona codigo y tiempo

---
+++ FibonacciCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace FibonacciCSharp
+//{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime tiempo1 = DateTime.Now;
+
+            int i, n, f1 = 0, f2 = 1, res = 0;
+            Console.Write("Introduzca un numero n para marcar el limite de la sucesion: \n");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("la sucesion resultante es:");
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            for (i = 0; i <= n-3; i++)
+            {
+                res = f1 + f2;
+                Console.WriteLine(res);
+                f1 = f2;
+                f2 = res;
+            }
+            DateTime tiempo2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+            Console.Write("Tiempo en segundos: " + total.ToString());
+            Console.ReadLine();
+        }
+    }
+//}
